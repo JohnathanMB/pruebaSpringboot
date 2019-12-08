30681530356086b3651a23162d1f37182bded3c5
Johnathan Monsalve <ja.monsalve.bello@gmail.com>
2019-12-08 18:13:35 -0500

    inicio de applicación

---
+++ src/test/java/co/com/devco/prueba/pruebaspringboot/PruebaSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.devco.prueba.pruebaspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
